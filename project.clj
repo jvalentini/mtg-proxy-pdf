ec5d36719539f5f4c87937115526cadffe0cbabb
Justin Valentini <justin.valentini@gmail.com>
2014-03-08 19:26:36 -0500

    Prototype.
    - Converts a given data struct to an html or pdf.
    - TODO: use templates for html
    - TODO: parse a text file / string for input instead of data struct

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [enlive "1.1.5"]
+                 [clj-pdf "1.11.15"]
+                 [ring/ring-codec "1.0.0"]
+                 [hiccup "1.0.5"]])
--- test/mtg_proxy_pdf/core_test.clj
+++ test/mtg_proxy_pdf/core_test.clj
@@ -1,7 +1,70 @@
 (ns mtg-proxy-pdf.core-test
   (:require [clojure.test :refer :all]
-            [mtg-proxy-pdf.core :refer :all]))
+            [mtg-proxy-pdf.core :refer :all]
+            [net.cgrand.enlive-html :as enlive]
+            [ring.util.codec :as codec]
+            [clj-pdf.core :as pdf]
+            [hiccup.core :as hiccup]
+            [hiccup.element :as element]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
+  (testing "it returns a list of card images"
     (is (= 0 1))))
+
+(def test-template "test/templates/academy-rector.html")
+
+(def test-card-name "Academy Rector")
+
+(def query-url "http://magiccards.info/query?q=Academy+Rector&v=card&s=cname")
+
+(def decklist [{ :name "Academy Rector", :quantity 1 }
+               { :name "Birthing Pod",   :quantity 1 }
+               { :name "Kitchen Finks",  :quantity 1 }])
+
+(defn sanitize [name]
+  (codec/url-encode name))
+
+(sanitize test-card-name)
+
+(defn build-query-url [card-record]
+  (let [{:keys [name]} card-record]
+    (format "http://magiccards.info/query?q=%s&v=card&s=cname" (sanitize name))))
+
+(build-query-url (first decklist))
+
+(defn image-url [card-name query-url]
+  (-> query-url
+      (java.net.URL.)
+      (enlive/html-resource)
+      (enlive/select [[:img (enlive/attr= :alt card-name)]])
+      (first)
+      (:attrs)
+      (:src)))
+
+(image-url (:name (first decklist)) (build-query-url (first decklist)))
+
+(defn decklist->images-urls [decklist]
+  (let [names (map :name decklist)
+        urls (map build-query-url decklist)]
+    (map image-url names urls)))
+
+(decklist->images-urls decklist)
+
+;; todo: include css to set the margin and padding
+;; todo: use template
+
+(defn images->html [images file-name]
+  (spit file-name (hiccup/html (map (fn [image] (element/image { :width 222 :height 315} image)) images))))
+
+(images->html (decklist->images-urls decklist) "test.html")
+
+(defn images->pdf [images file-name]
+  (pdf/pdf
+   [{}
+    (map (fn [image] [:image {:xscale 0.5
+                              :yscale 0.5
+                              :align  :center}
+                      image]) images)]
+   file-name))
+
+(images->pdf (decklist->images-urls decklist) "test.pdf")
