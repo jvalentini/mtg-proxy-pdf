887de7ce149f637c3a0e15b580df1198662541bd
Justin Valentini <justin.valentini@gmail.com>
2014-03-09 15:27:51 -0400

    Extract parse-record function.

--- src/mtg_proxy_pdf/decklist_parser.clj
+++ src/mtg_proxy_pdf/decklist_parser.clj
@@ -1,11 +1,14 @@
 (ns mtg-proxy-pdf.decklist-parser
   (:require [clojure.string :refer [split capitalize join]]))
 
+(defn parse-record
+  [record]
+  (let [[_ quantity card-name] (re-find #"(^\d)?x?\s*(.*)" record)]
+    [(Integer. (or quantity 1)) (join " " (map capitalize (split card-name #" ")))]))
+
 (defn parse-card-name-quantity
   [record]
-  (let [[_ quantity card-name] (re-find #"(^\d)?x?\s*(.*)" record)
-        quantity (Integer. (or quantity 1))
-        card-name (join " " (map capitalize (split card-name #" ")))]
+  (let [[quantity card-name] (parse-record record)]
     { :quantity quantity :name card-name }))
 
 ;; TODO: should parse out a quantity
