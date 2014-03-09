21ed127a9ba6f6ad452b402da097c52688bb0b12
Justin Valentini <justin.valentini@gmail.com>
2014-03-09 11:53:47 -0400

    Move functions to core.clj:
    - build-query-url
    - image-url
    - decklist->images-urls

--- src/mtg_proxy_pdf/core.clj
+++ src/mtg_proxy_pdf/core.clj
@@ -1,1 +1,27 @@
-(ns mtg-proxy-pdf.core)
+(ns mtg-proxy-pdf.core
+  (:require [net.cgrand.enlive-html :as enlive]
+            [ring.util.codec :as codec]
+            [clj-pdf.core :as pdf]
+            [hiccup.core :as hiccup]
+            [hiccup.element :as element]))
+
+;; todo: include css to set the margin and padding
+;; todo: use template
+
+(defn build-query-url [card-record]
+  (let [{:keys [name]} card-record]
+    (format "http://magiccards.info/query?q=%s&v=card&s=cname" (codec/url-encode name))))
+
+(defn image-url [card-name query-url]
+  (-> query-url
+      (java.net.URL.)
+      (enlive/html-resource)
+      (enlive/select [[:img (enlive/attr= :alt card-name)]])
+      (first)
+      (:attrs)
+      (:src)))
+
+(defn decklist->images-urls [decklist]
+  (let [names (map :name decklist)
+        urls (map build-query-url decklist)]
+    (map image-url names urls)))
--- test/mtg_proxy_pdf/core_test.clj
+++ test/mtg_proxy_pdf/core_test.clj
@@ -1,14 +1,6 @@
 (ns mtg-proxy-pdf.core-test
   (:require [clojure.test :refer :all]
-            [mtg-proxy-pdf.core :refer :all]
-            [net.cgrand.enlive-html :as enlive]
-            [ring.util.codec :as codec]
-            [clj-pdf.core :as pdf]
-            [hiccup.core :as hiccup]
-            [hiccup.element :as element]))
-
-;; todo: include css to set the margin and padding
-;; todo: use template
+            [mtg-proxy-pdf.core :refer :all]))
 
 (def test-template "test/templates/academy-rector.html")
 (def test-query-url "http://magiccards.info/query?q=Academy%20Rector&v=card&s=cname")
@@ -20,39 +12,17 @@
                     { :name "Birthing Pod",   :quantity 1 }
                     { :name "Kitchen Finks",  :quantity 1 }])
 
-(defn build-query-url [card-record]
-  (let [{:keys [name]} card-record]
-    (format "http://magiccards.info/query?q=%s&v=card&s=cname" (codec/url-encode name))))
-
 (deftest build-query-url-test
   (testing "it builds a url to magiccards.info"
-    (is (= test-query-url (build-query-url test-card-record)))))
-
-(defn image-url [card-name query-url]
-  (-> query-url
-      (java.net.URL.)
-      (enlive/html-resource)
-      (enlive/select [[:img (enlive/attr= :alt card-name)]])
-      (first)
-      (:attrs)
-      (:src)))
-
-(image-url (:name (first test-decklist)) (build-query-url (first test-decklist)))
+    (is (= test-query-url (mtg-proxy-pdf.core/build-query-url test-card-record)))))
 
 (deftest image-url-test
   (testing "it returns the url to the card image"
-    (is (= test-image-url (image-url test-card-name test-query-url)))))
-
-(defn decklist->images-urls [decklist]
-  (let [names (map :name decklist)
-        urls (map build-query-url decklist)]
-    (map image-url names urls)))
-
-(decklist->images-urls test-decklist)
+    (is (= test-image-url (mtg-proxy-pdf.core/image-url test-card-name test-query-url)))))
 
 (deftest decklist->images-urls-test
   (testing "it builds a url to magiccards.info"
-    (is (= test-image-url-list (decklist->images-urls test-decklist)))))
+    (is (= test-image-url-list (mtg-proxy-pdf.core/decklist->images-urls test-decklist)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; BEWARE: SIDE-EFFECTS LIVE BELOW!!!
@@ -61,7 +31,7 @@
 (defn images->html [images file-name]
   (spit file-name (hiccup/html (map (fn [image] (element/image { :width 222 :height 315} image)) images))))
 
-(images->html (decklist->images-urls test-decklist) "test.html")
+(images->html (mtg-proxy-pdf.core/decklist->images-urls test-decklist) "test.html")
 
 (defn images->pdf [images file-name]
   (pdf/pdf
@@ -72,4 +42,4 @@
                       image]) images)]
    file-name))
 
-(images->pdf (decklist->images-urls test-decklist) "test.pdf")
+(images->pdf (mtg-proxy-pdf.core/decklist->images-urls test-decklist) "test.pdf")
