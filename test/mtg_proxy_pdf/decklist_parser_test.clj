f5c6078a37bd9c63f0ed34e4b340f2f5d81eab43
Robert Lambert <Robert.Lambert@xls.xerox.com>
2014-03-12 23:17:15 -0400

    updated tests to reflect new deck lists

--- test/mtg_proxy_pdf/decklist_parser_test.clj
+++ test/mtg_proxy_pdf/decklist_parser_test.clj
@@ -3,12 +3,15 @@
             [mtg-proxy-pdf.decklist-parser :refer :all]
             [clojure.java.io :as io]))
 
-(def test-decklist-string "Academy Rector\nBirthing Pod\n2 Kitchen Finks")
-(def test-comma-separated-decklist-string "Academy Rector,Birthing Pod,2x Kitchen Finks")
-(def test-dash-separated-decklist-string "Academy Rector---Birthing Pod---2   Kitchen Finks")
-(def test-decklist [{ :name "Academy Rector", :quantity 1 }
-                    { :name "Birthing Pod",   :quantity 1 }
-                    { :name "Kitchen Finks",  :quantity 2 }])
+(def test-decklist-string "Academy Rector\nBirthing Pod\n2 Kitchen Finks\n3* Fall of the Hammer\n2 Lich's Mirror\n12 Mana Flair")
+(def test-comma-separated-decklist-string "Academy Rector,Birthing Pod,2x Kitchen Finks,3* Fall of the Hammer,2 Lich's Mirror,12 Mana Flair")
+(def test-dash-separated-decklist-string "Academy Rector---Birthing Pod---2   Kitchen Finks---3* Fall of the Hammer---2 Lich's Mirror---12 Mana Flair")
+(def test-decklist [{ :name "Academy Rector",     :quantity 1 }
+                    { :name "Birthing Pod",       :quantity 1 }
+                    { :name "Kitchen Finks",      :quantity 2 }
+                    { :name "Fall Of The Hammer", :quantity 3 }
+                    { :name "Lich's Mirror",      :quantity 2 }
+                    { :name "Mana Flair",         :quantity 12 }])
 
 (deftest parse-card-name-no-quantity-test
   (testing "it parses a card name with no quantity"
