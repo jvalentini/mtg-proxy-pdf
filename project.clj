60661f2c3ca7f7b867e4e68fd8bf77e8c95ea524
Robert Lambert <Robert.Lambert@xls.xerox.com>
2014-03-15 01:22:56 -0400

    Updated description, and project URL, added development dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mtg-proxy-pdf "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Given a list of magic cards, create a file (html or pdf) of the card images."
+  :url "https://github.com/jvalentini/mtg-proxy-pdf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -8,4 +8,5 @@
                  [clj-pdf "1.11.15"]
                  [ring/ring-codec "1.0.0"]
                  [hiccup "1.0.5"]
-                 [org.clojure/data.json "0.2.4"]])
+                 [org.clojure/data.json "0.2.4"]]
+  :dev-dependencies [[org.clojure/tools.trace "0.7.6"]])
